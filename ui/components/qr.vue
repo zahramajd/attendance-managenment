729d9a29bd6cb8cd2bf56540eda2910a41c4ae12
zahramajd <zahra.majdabadi95@gmail.com>
2017-08-28 16:49:45 +0430

    qr component

---
+++ ui/components/qr.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <img :src="qrImageSrc">
+        <br> {{ token }}
+    </div>
+</template>
+
+<style scoped>
+.container {
+    text-align: center;
+    display: block;
+    padding: 5px;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+</style>
+
+<script>
+import totpToken from 'otplib/core/totpToken'
+
+export default {
+    data() {
+        return {
+            token: ''
+        }
+    },
+    props: {
+        secret: String,
+        bgColor: {
+            type: String,
+            default: 'white'
+        },
+        size: {
+            type: Number,
+            default: 200
+        }
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            this.token = totpToken(this.secret, { step: 30 })
+        }, 1000)
+    },
+    destroy() {
+        clearInterval(this.interval)
+    },
+    computed: {
+        qrImageSrc() {
+            return `https://api.qrserver.com/v1/create-qr-code/?size=${this.size}x${this.size}&data=${this.token}&bgcolor=${this.bgColor}`
+        }
+    }
+}
+</script>
--- ui/pages/device/_id/edit.vue
+++ ui/pages/device/_id/edit.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
         <br>
-        <b-card :title="device.name" :sub-title="device.secret"> </b-card>
+
+        <b-card :title="device.name" :sub-title="device.secret">
+            <div class="row">
+                <div class="col-3">
+                    <qr :secret="device.secret" />
+                </div>
+                <div class="col-8">
+                    <pre>{{ JSON.stringify(device, undefined, 8) }}</pre>
+                </div>
+            </div>
+        </b-card>
     </div>
 </template>
 
 <script>
-export default {
+import totpToken from 'otplib/core/totpToken'
+import qr from '~/components/qr'
 
+export default {
+    components: { qr },
     data() {
         return {
             device: {}
