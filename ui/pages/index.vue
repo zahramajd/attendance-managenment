9b59b588d814d1e112868111f429ac967b5a94f0
zahramajd <zahra.majdabadi95@gmail.com>
2017-10-19 21:26:27 +0330

    index page

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -5,18 +5,18 @@
             <br>
             <b-card-group deck class="mb-3">
                 <b-card bg-variant="primary" text-variant="white" header="کاربران" class="text-center">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p class="card-text">مشاهده و ویرایش اطلاعات کاربران</p>
                 </b-card>
                 <b-card bg-variant="danger" text-variant="white" header="دروس" class="text-center">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p class="card-text">مشاهده و ویرایش اطلاعات درس‌ها</p>
                 </b-card>
             </b-card-group>
             <b-card-group deck class="mb-3">
                 <b-card bg-variant="success" text-variant="white" header="دستگاه ها" class="text-center">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p class="card-text">مشاهده و ویرایش اطلاعات دستگاه‌ها</p>
                 </b-card>
                 <b-card bg-variant="warning" text-variant="white" header="گزارش ها" class="text-center">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p class="card-text">مشاهده گزارش ها</p>
                 </b-card>
             </b-card-group>
 
