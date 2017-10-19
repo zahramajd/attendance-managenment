a43ac1d0042ca6807413bddde3fca25f6cba2ffe
zahramajd <zahra.majdabadi95@gmail.com>
2017-10-19 21:40:25 +0330

    translate header

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -8,7 +8,7 @@
 
       <b-collapse is-nav id="nav_collapse">
         <b-nav is-nav-bar>
-          <b-nav-item to="/" exact>Home</b-nav-item>
+          <b-nav-item to="/" exact>خانه</b-nav-item>
           <b-nav-item to="/users" v-if="$store.getters.accessUsers">کاربران</b-nav-item>
           <b-nav-item to="/sessions" v-if="$store.getters.accessSessions">درس‌ها</b-nav-item>
           <b-nav-item to="/devices" v-if="$store.getters.accessDevices">دستگاه‌ها</b-nav-item>
